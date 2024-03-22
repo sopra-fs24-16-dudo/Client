fe5f370b1a275659d97b1c312129ab9445431e75
Jamin Sulic <jamin.sulic@uzh.ch>
2024-03-22 22:17:53 +0100

    1st page (look)

--- src/components/views/Login.tsx
+++ src/components/views/Login.tsx
@@ -41,14 +41,14 @@ const Login = () => {
     try {
       const requestBody = JSON.stringify({ username, name });
       const response = await api.post("/login", requestBody);
-      
+
       const user = new User(response.data);
-      
+
       localStorage.setItem("token", user.token);
       localStorage.setItem("id", user.id);
 
-      
-      navigate("/game"); 
+
+      navigate("/game");
     } catch (error) {
       alert(
         `Something went wrong during the login: \n${handleError(error)}`
@@ -58,45 +58,60 @@ const Login = () => {
 
   const doRegistration = () => {
     navigate("/registration");
-  } 
+  }
 
   return (
-    <BaseContainer>
-      <div className="login container">
-        <div className="login form">
-          <FormField
-            placeholder="Username"
-            value={username}
-            onChange={(un: string) => setUsername(un)}
-          />
-          <FormField
-            placeholder="Password"
-            value={name}
-            onChange={(n) => setName(n)}
-          />
-          <div className="login button-container">
-            <Button
-              disabled={!username || !name}
-              width="100%"
-              onClick={() => doLogin()}
-            >
-              Login
-            </Button>
-            <Button
-              width="100%"
-              onClick={() => doRegistration()}
-              className="secondary-button"
-            >
-              Registration
-            </Button>
+    <div className="Div">
+      <div className="Div2">
+        <div className="Div3">
+          <div className="Div4">
+            <div className="Column">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Fb3223ee50ca84556b9540adede97590c?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Fb3223ee50ca84556b9540adede97590c?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Fb3223ee50ca84556b9540adede97590c?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Fb3223ee50ca84556b9540adede97590c?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Fb3223ee50ca84556b9540adede97590c?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Fb3223ee50ca84556b9540adede97590c?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Fb3223ee50ca84556b9540adede97590c?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Fb3223ee50ca84556b9540adede97590c"
+                className="Img"
+              />
+            </div>
+            <div className="Column2">
+              <div className="Div5">
+              <span style={{
+                fontFamily: "Libre Bodoni",
+                fontSize: "100px",
+                fontStyle: "normal",
+                fontVariantLigatures: "normal",
+                fontVariantCaps: "normal",
+                fontWeight: 400
+              }}>
+                Dudo
+              </span>
+              </div>
+            </div>
+            <div className="Column3">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Ff61ebad238f2460fa82781fbaf0546cc?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Ff61ebad238f2460fa82781fbaf0546cc?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Ff61ebad238f2460fa82781fbaf0546cc?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Ff61ebad238f2460fa82781fbaf0546cc?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Ff61ebad238f2460fa82781fbaf0546cc?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Ff61ebad238f2460fa82781fbaf0546cc?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Ff61ebad238f2460fa82781fbaf0546cc?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2Fc5149f80998544599b064363e6cf88da%2Ff61ebad238f2460fa82781fbaf0546cc"
+                className="Img2"
+              />
+            </div>
           </div>
         </div>
+        <div className="Div6">
+          <br />
+          <span style={{ fontFamily: "Libre Bodoni", fontSize: "50px" }}>
+          Roll, Bid, and Bluff Your Way to Victory
+        </span>
+        </div>
+        <div className="Div7">
+          <div className="Div8">Username</div>
+          <div className="Div9" />
+          <div className="Div10">login</div>
+        </div>
       </div>
-    </BaseContainer>
+    </div>
   );
 };
 
-/**
+  /**
  * You can get access to the history object's properties via the useLocation, useNavigate, useParams, ... hooks.
  */
 export default Login;
