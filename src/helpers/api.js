1e06e1eb7069d5c1e6e4ccde1634e682a97b5d55
kkuneva <120613597+kkuneva@users.noreply.github.com>
2024-05-08 09:52:39 +0200

    Update api.js
    Fix error expected a newline before return statement

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -24,6 +24,7 @@ export const handleError = error => {
     }
 
     console.log("The request was made and answered but was unsuccessful.", error.response);
+    
     return info;
   } else {
     if (error.message.match(/Network Error/)) {
@@ -31,6 +32,7 @@ export const handleError = error => {
     }
 
     console.log("Something else happened.", error);
+    
     return error.message;
   }
 };
